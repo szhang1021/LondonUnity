6161b0ca422c74d2289abaec5095a632a7ef9da8
szhang1021 <52924435+szhang1021@users.noreply.github.com>
2021-02-22 00:18:02 +0800

    just updating

---
+++ London/Assets/Scripts/Jumping.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumping : MonoBehaviour
+{
+
+	private Rigidbody rb;
+    public float speed;
+    public int jumpforce;
+    public int gravityforce;
+    private Vector3 jump;
+
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+        jump = new Vector3(0,1,0);
+        Physics.gravity = new Vector3(0,-gravityforce,0);
+	}
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        bool moveUp = Input.GetKey("space");
+
+
+        if (moveUp == true){
+            rb.velocity = new Vector3(0,jumpforce,0);
+        }
+    }
+}
---
+++ London/Assets/Scripts/PathFollowing.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class PathFollowing : MonoBehaviour
+{
+   public PathCreator pathcreator;
+   public float speed = 5;
+   float distanceTravelled;
+    // Update is called once per frame
+    void Update()
+    {
+        distanceTravelled += speed * Time.deltaTime;
+        transform.position = pathcreator.path.GetPointAtDistance(distanceTravelled);
+        //transform.rotation = pathcreator.path.GetRotationAtDistance(distanceTravelled);
+    }
+}
